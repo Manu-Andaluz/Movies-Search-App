b598deb8c97db2ecb2fb4cca87aba96ef1b656ea
manu-andaluz <manupeandaluz@gmail.com>
2022-10-14 21:28:00 -0300

    adding more styling

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom";
 export class ConnectedList extends Component {
   render() {
     return (
-      <div className="grid place-content-center">
+      <div className="grid place-content-center p-5">
         <h2 className="text-2xl mt-4">Favorite Movies</h2>
         <ul>
           {this.props.movies &&
             this.props.movies.map((movie) => (
               <div key={movie.id}>
-                <Link to={`/movie/:$${movie.id}`}>{movie.title}</Link>
+                <Link to={`/movie/:${movie.id}`}>{movie.title}</Link>
                 <button
                   onClick={() => this.props.removeMovieFavorite(movie.id)}
                   className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 m-5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { getMovieDetail } from "../../actions/index";
-
-import "./Movie.css";
 
 export default function Movie() {
   let movieID = useParams();
@@ -20,11 +18,43 @@ export default function Movie() {
   const state = useSelector((state) => state.movieDetail);
 
   return (
-    <div className="movie-detail">
-      <h1>{state.Title}</h1>
-      <h2>{state.Director}</h2>
-      <h3>{state.Year}</h3>
+    <div className="flex m-auto p-5">
       <img src={state.Poster} />
+
+      <div className="grid gap-3 place-content-center mt-3 p-4">
+        <h1 className="">{state.Title}</h1>
+        <h2>Plot: {state.Plot}</h2>
+
+        <div className="flex">
+          <h2 className="mr-4">Director: {state.Director}</h2>
+          <h3>Cast: {state.Actors}</h3>
+        </div>
+        <div className="flex">
+          <h4 className="mr-4">Genre: {state.Genre}</h4>
+          <h4>Release Year: {state.Year}</h4>
+        </div>
+
+        <h4>{state.Runtime}</h4>
+        <p>{state.Awards}</p>
+        <p>{state.imdbRating}</p>
+      </div>
     </div>
   );
 }
+
+/*
+"Ratings": [
+{
+"Source": "Internet Movie Database",
+"Value": "6.7/10"
+},
+{
+"Source": "Rotten Tomatoes",
+"Value": "69%"
+},
+{
+"Source": "Metacritic",
+"Value": "59/100"
+}
+],
+*/
